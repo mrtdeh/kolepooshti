79e1cb7fa90ec8d6f1c40151bda5ed9fb315132a
Morteza Dehghani <mrtdeh@gmail.com>
2021-01-21 02:36:11 +0330

    addCourse method

--- app/Http/Controllers/MeetingController.php
+++ app/Http/Controllers/MeetingController.php
@@ -141,7 +141,7 @@ class MeetingController extends Controller
         // Fetch All course Times for this User
         $rooms = $user->rooms()->get();
         // dd(  $user->id);
-      dd($user->rooms()->distinct());
+      dd($user->rooms()->distinct()->get());
         foreach ($rooms as $key => $room) {
 
             // dd($room->schedules);
