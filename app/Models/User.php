239e8b1ff116f8cf65ce45affca473d37abd0a0e
Morteza Dehghani <mrtdeh@gmail.com>
2021-01-21 02:34:12 +0330

    addCourse method

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,8 +46,8 @@ class User extends Authenticatable
         if ($this->attributes["type"] == "teacher")
 
             return $this->belongsToMany( ClassRoom::class , "class_course_schedule" , "teacher_id", "class_id")
-            ->withPivot("schedule_id")
-            ->where("schedule_id","!=","")->distinct();
+            ->distinct()->withPivot("schedule_id")
+            ->where("schedule_id","!=","");
             // return DB::table("class_course_schedule")->where("teacher_id","=",$id);
 
         return $this->belongsToMany( ClassRoom::class , "class_user" , "user_id" ,"class_id");
